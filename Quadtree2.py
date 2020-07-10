38ff2ef34ac8ee2fef00042a691bc8b272ddf406
Jonathan <jonathan.olsalazar@outlook.com>
2020-07-10 22:05:23 +1000

    Added inital functionality for Insert method

--- Quadtree2.py
+++ Quadtree2.py
@@ -1,11 +1,11 @@
 
 # Class for point in Quadtree
 class Point:
-    def __init__(self, latitude, longitude, altitude, timestamp): # long = x, lat = y
+    def __init__(self, longitude, latitude, altitude, time): # long = x, lat = y
         self.latitude = latitude
         self.longitude = longitude
         self.altitude = altitude
-        self.timestamp = timestamp
+        self.time = time
     
     # Add GETTERS/SETTERS
 
@@ -14,24 +14,29 @@ class Point:
         # Internal Node - Has 4 children, cannot have a point
         # Leaf Node - No children, can have a point
     # Node can switch between types
+    # Node can only store points with same coords
 class Node:
     # New nodes will be type Leaf and store no points
-    def __init__(self, bL, tR, points={}, children={}, root=0): 
+    def __init__(self, bL, tR, points=[], children=[], root=0): 
         self.points = points
         self.children = children
         self.root = root
     
     # ADD GETTERS/SETTERS
+    def addPoint(self, newPoints):
+        self.points.append(newPoints)
 
 # Class for Quadtree
 class Quadtree:
     # Give initial size of Quadtree
     # Creates root node
-    def __init__(self, latitude1, longitude1, latitude2, longitude2): # 1 = bottom left corner, 2 = top right corner
+    def __init__(self, latitude1, longitude1, latitude2, longitude2, maxPoints): # 1 = bottom left corner, 2 = top right corner
         bL = [longitude1, latitude1] # Bottom left corner (X,Y)
         tR = [longitude2, latitude2] # Top right corner (X,Y)
-        self.root = Node(bL, tR, root = 1)
 
+        self.maxPoints = maxPoints # Max points before decomposition
+        self.root = Node(bL, tR, root = 1) # Create root node
+        
 
     # Split node into 4 new nodes
         # Move points to corresponding nodes
@@ -41,8 +46,12 @@ class Quadtree:
 
 
     # Insert a point into a node
-    def Insert(self):
-        pass 
+    def Insert(self, longitude, latitude, altitude, time):
+        # Create a point object
+        point = Point(longitude,latitude, altitude, time)
+
+        # Add point to tree
+        self.root.addPoint(point)
 
     # Update existing node
     def Update(self):
@@ -63,10 +72,11 @@ class Quadtree:
 def main():
 
     # Initialize Quadtree
-    quadtree = Quadtree(0,0,10,10)
+    quadtree = Quadtree(0,0,10,10, 1)
 
+    quadtree.Insert(1,1,2,3)
 
-
+    x = 1
 if __name__ == "__main__":
     # execute only if run as a script
     main()  
