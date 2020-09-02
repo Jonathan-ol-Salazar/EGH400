ef573ce018abc94c31600d7c1afc04916a47deda
Jonathan-ol-Salazar <jonathan.olsalazar@outlook.com>
2020-09-02 13:58:01 +1000

    Added feature for maximum parameters

--- Flight_Plan_Generator.py
+++ Flight_Plan_Generator.py
@@ -8,20 +8,40 @@ import json
 # Altitude = 1 meter
 
 
-# Parameters
-duration = 10  # Seconds
-distance = 1  # Meters
-altitude = 1  # Meters
+# Initial Parmeters (10 seconds, 1 meter, 1 meter)
+duration = 10  
+distance = 1  
+altitude = 1  
+
+# Maximum Parmeters (600 seconds (10 mins), 1 km, 20m)
+duration = 600  
+distance = 1000
+altitude = 20  
+
+# Start Coords (Only X, because Y = altitude)
+startX = 0
 
 # Example Times, ISO-8601
 startTime = "2020-07-29T00:00:00Z"
-endTime =   "2020-07-29T00:00:10Z"
+endTime =   "2020-07-29T00:10:00Z"
 
 locationAMSL = "42,-70,0' AMSL" 
 radius = "1m" 
-height = "1m"
+height = "20m"
 
-points = [[1,1], [1,2]]
+# Spacing between points
+spacing = 20
+
+# For loop to make a list of tuples containing coords. X will be 'distance' split into 20m increments, Y is altitude
+points = [(startX, altitude)]
+# Check distance is not 0
+if distance != 0:
+    for i in range(1, int(distance/spacing)+1):
+        points.append((i*spacing, altitude))
+
+
+
+# points = [[1,1], [1,2]]
 
 dictJSON = {"start_time": startTime, "expire_time": endTime, "location": locationAMSL, "points": points, "altitude":altitude}
 
